6929a7b6e76579273ef77b820a1d2d0e384007ba
MONICA <mmdiazmoreno@gmail.com>
2023-11-24 22:37:56 -0500

    Se actualiza

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,15 +1,34 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Navbar = () => {
 
+  
   return (
-    <nav>
+    
+    <nav className='navbar'>
       {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
+      <ul className='menu'>
+        <li>
+            <Link to='/home'>Home</Link>
+        </li>
+        <li>
+          <Link to='/detail'>Detail</Link>
+        </li>
+        <li>
+          <Link to='/fav'>Favorite</Link>
+        </li>
+        <li>
+          <Link to='/contact'>Contact</Link>
+        </li>
+      </ul>
+
       {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
       <button>Change theme</button>
-    </nav>
+    </nav>    
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Routes/Home'
+import Favs from './Routes/Favs'
+import Contact from './Routes/Contact'
+import Detail from './Routes/Detail'
+import NotFount from './Routes/NotFount';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />}  >
+          <Route path='/home' element={<Home />} />
+          <Route path='/detail/:id?' element={<Detail />} />
+          <Route path='/fav' element={<Favs />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='*' element={<NotFount />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+
+  </React.StrictMode>
+);
