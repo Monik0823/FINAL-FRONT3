a76d55251796e983244f041cbba116dcc0283807
MONICA <mmdiazmoreno@gmail.com>
2023-11-25 23:48:57 -0500

    Se actualiza el form

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -6,23 +6,36 @@ const Form = () => {
   const [nombre, setNombre] = useState("");
   const [email, setEmail] = useState("");
   const [errorEmail, setErrorEmail] = useState(false);
+  const [errorNombre, setErrorNombre] = useState(false);
   const [resultado, setResultado] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
+  
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-    if (nombre.length <= 5 || !emailRegex.test(email)) {
-      alert("Por favor verifique su información nuevamente")
-    } 
-    if (!emailRegex.test(email)) {
-      setErrorEmail(true);
-    }else {
-      const mensaje = `¡¡ Gracias ${nombre}, te contactaremos cuanto antes vía mail !!`;
-      setResultado(mensaje);
+  
+    let valid = true;
+  
+    if (nombre.trim().length <= 5 || /^\s|\s$/.test(nombre)) {
+      setErrorNombre(true);
+      valid = false;
+    } else {
+      setErrorNombre(false);
     }
-
+  
+    if (!emailRegex.test(email) || /\s/.test(email)) {
+      setErrorEmail(true);
+      valid = false;
+    } else {
+      setErrorEmail(false);
+    }
+  
+    if (valid) {
+      const mensaje = `¡Gracias ${nombre}, te contactaremos pronto vía email!`;
+      setResultado(mensaje);
+    } else {
+      setResultado("");
+    }
   };
 
   return (
@@ -35,8 +48,14 @@ const Form = () => {
             placeholder="Ingresa tu nombre completo"
             id='nombre'
             value={nombre}
-            onChange={e => setNombre(e.target.value)}
+            onChange={(e) => {
+              setNombre(e.target.value);
+              setErrorNombre(false);
+            }}
           />
+          {errorNombre && (
+            <p className="mensaje-error">El nombre debe tener más de 5 caracteres y no contener espacios en blanco.</p>
+          )}
         </div>
         <div>
           <label htmlFor="email">Email: </label>
@@ -45,10 +64,14 @@ const Form = () => {
             placeholder="Ingresa tu email"
             id='email'
             value={email}
-            onChange={(e) => {setEmail(e.target.value)
-              setErrorEmail(false)
+            onChange={(e) => {
+              setEmail(e.target.value);
+              setErrorEmail(false);
             }}
           />
+          {errorEmail && (
+            <p className="mensaje-error">Por favor ingresa un email válido sin espacios en blanco.</p>
+          )}
         </div>
         <div>
           <button type='submit'>
